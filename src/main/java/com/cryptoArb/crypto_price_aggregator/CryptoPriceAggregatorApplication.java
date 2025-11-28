28b72ff83ca07c92dbc72ecdbed822882f31e4ae
pranay-a-1 <pranay.a4795@gmail.com>
2025-11-28 16:28:11 +0530

    feat: Initialize crypto price aggregator project with Spring Boot, Maven Wrapper, and updated gitignore.

---
+++ src/main/java/com/cryptoArb/crypto_price_aggregator/CryptoPriceAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.cryptoArb.crypto_price_aggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoPriceAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoPriceAggregatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cryptoArb/crypto_price_aggregator/CryptoPriceAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptoArb.crypto_price_aggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoPriceAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
